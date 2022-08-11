bf37168d9a89bb4a569fd51ac40b4053c52ccd62
ZdenZ <dog123fri@mail.ru>
2022-08-11 22:15:30 +0300

    second commit

--- learnJs/object.js
+++ learnJs/object.js
@@ -5,11 +5,34 @@ const options = {
     colors: {
         border: 'balck',
         bg: 'white'
+    },
+    makeTest: function(){
+        console.log('Колибровка')
     }
 }
 
-console.log(options.width)
+// Деструктуризация 
 
-delete options.heigh
+const {border, bg} = options.colors
+console.log(bg)
+options.makeTest()
 
-console.log(options)
+console.log(Object.keys(options).length)
+
+// let counter = 0
+
+//     for(let key in options){
+//         if(typeof(options[key]) === 'object'){
+//             for(let i in options[key]){
+//                 console.log(`Полю ${i} соответсвует значение ${options[key][i]}`)
+//                 counter++
+//             }
+//         }else{
+//             console.log(`Полю ${key} соответсвует значение ${options[key]}`)
+//             counter++
+//         }
+//     }
+// console.log(counter)
+
+
+
--- main.js
+++ main.js
@@ -11,8 +11,7 @@ const bool =  true;
 console.log(4/0);
 //Nan 
 console.log('sad'*3);
-//null - переменной не существует 
-console.log(Human);
+//null - переменной не существует   
 //undefind
 let validator;
 // object, array- частный случай объекта
@@ -45,3 +44,80 @@ const storeDescription = {
     open : true
 }
 
+// Работа с массивами 
+
+const arr = [1, 3, 5, 7, 2]
+
+arr.sort(compare)
+// Быстрая сортировка алгоритм 
+function compare(a, b) {
+    return a - b
+}
+
+console.log(arr)
+// arr.pop()
+
+// console.log(arr)
+
+// arr.push(12)
+
+// console.log(arr)    
+
+// for ( let i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+
+// for (let value of arr){
+//     console.log(value)
+// }
+
+arr.forEach(function(item, i, arr) {
+    console.log(` иднекс элемента ${i} = значению  ${item} из массива ${arr}`)
+})
+
+
+const Ask = prompt("Введите фрукты через запятую", "")
+
+const product = Ask.split(', ');
+
+console.log(product)
+
+
+// Передача по ссылке и по значению
+
+let a = 5,
+    b = a;
+   
+b = b + 5
+
+console.log(b)
+console.log(a)
+
+const obj = {
+    fName: 'Alice',
+    lName: 'Vrunder',
+    clothes: {
+        chest: 'blouse',
+        sneakers: 'adidas'
+    }
+}
+
+// function copyrated (value){
+//     let copy = {}
+//     for(let key in value){
+//         copy[key] = value[key];
+//     }return copy
+// }
+
+// const person = copyrated(obj)
+// person.fName = 'Slame'
+
+// console.log(copyrated(person))
+// console.log(obj)
+
+const add = {
+    hair: 'brown'
+}
+
+const colorHair = (Object.assign({},add))   
+console.log(colorHair)
